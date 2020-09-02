d42c16a787ccff00f5637860a5cdbf532e5c6cff
haganmao <kevin_mmc@hotmail.com>
2020-09-02 14:39:29 +1200

    update folder

---
+++ QMRestaurantBooking/FinalProject/FinalProject/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace FinalProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboboxTableNumber = new System.Windows.Forms.ComboBox();
+            this.numPeople = new System.Windows.Forms.NumericUpDown();
+            this.btnnew = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtSeacrchName = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataSet1 = new System.Data.DataSet();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtBookingID = new System.Windows.Forms.TextBox();
+            this.comboTime = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPeople)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            this.label8.Click += new System.EventHandler(this.label8_Click);
+            // 
+            // txtname
+            // 
+            resources.ApplyResources(this.txtname, "txtname");
+            this.txtname.Name = "txtname";
+            this.txtname.TextChanged += new System.EventHandler(this.txtname_TextChanged);
+            // 
+            // txtPhone
+            // 
+            resources.ApplyResources(this.txtPhone, "txtPhone");
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.TextChanged += new System.EventHandler(this.txtPhone_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            resources.ApplyResources(this.dateTimePicker1, "dateTimePicker1");
+            this.dateTimePicker1.MinDate = new System.DateTime(2020, 1, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Value = new System.DateTime(2020, 6, 24, 23, 59, 59, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // comboboxTableNumber
+            // 
+            this.comboboxTableNumber.FormattingEnabled = true;
+            resources.ApplyResources(this.comboboxTableNumber, "comboboxTableNumber");
+            this.comboboxTableNumber.Name = "comboboxTableNumber";
+            this.comboboxTableNumber.SelectedIndexChanged += new System.EventHandler(this.tableNumber_SelectedIndexChanged);
+            // 
+            // numPeople
+            // 
+            resources.ApplyResources(this.numPeople, "numPeople");
+            this.numPeople.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numPeople.Name = "numPeople";
+            this.numPeople.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numPeople.ValueChanged += new System.EventHandler(this.numPeople_ValueChanged);
+            // 
+            // btnnew
+            // 
+            resources.ApplyResources(this.btnnew, "btnnew");
+            this.btnnew.Name = "btnnew";
+            this.btnnew.UseVisualStyleBackColor = true;
+            this.btnnew.Click += new System.EventHandler(this.btnnew_Click);
+            // 
+            // btnAdd
+            // 
+            resources.ApplyResources(this.btnAdd, "btnAdd");
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnUpdate
+            // 
+            resources.ApplyResources(this.btnUpdate, "btnUpdate");
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            resources.ApplyResources(this.btnDelete, "btnDelete");
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // txtSeacrchName
+            // 
+            resources.ApplyResources(this.txtSeacrchName, "txtSeacrchName");
+            this.txtSeacrchName.Name = "txtSeacrchName";
+            this.txtSeacrchName.TextChanged += new System.EventHandler(this.txtSeacrchName_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            resources.ApplyResources(this.dataGridView1, "dataGridView1");
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.RowTemplate.ReadOnly = true;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // btnSearch
+            // 
+            resources.ApplyResources(this.btnSearch, "btnSearch");
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
+            // 
+            // txtBookingID
+            // 
+            resources.ApplyResources(this.txtBookingID, "txtBookingID");
+            this.txtBookingID.Name = "txtBookingID";
+            this.txtBookingID.ReadOnly = true;
+            this.txtBookingID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboTime
+            // 
+            this.comboTime.FormattingEnabled = true;
+            resources.ApplyResources(this.comboTime, "comboTime");
+            this.comboTime.Name = "comboTime";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.comboTime);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtBookingID);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSeacrchName);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnnew);
+            this.Controls.Add(this.numPeople);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboboxTableNumber);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numPeople)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboboxTableNumber;
+        private System.Windows.Forms.NumericUpDown numPeople;
+        private System.Windows.Forms.Button btnnew;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSeacrchName;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtBookingID;
+        private System.Windows.Forms.ComboBox comboTime;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ QMRestaurantBooking/FinalProject/FinalProject/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        static string xmlPath  = "bookinginfo.xml";
+        static string bookingID = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //bind xml to gridview
+        private void loadXmlFile()
+        {
+            DataSet mybooking = new DataSet();
+            mybooking.ReadXml(xmlPath);
+            try
+            {
+                dataGridView1.DataSource = mybooking.Tables[0];
+                dataGridView1.Columns[7].Visible = false;
+            }
+            catch (System.Exception)
+            {
+
+            }
+        }
+
+
+        //add item to 
+        protected void Form1_Load_1(object sender, EventArgs e)
+        {
+            comboboxTableNumber.Items.Add("A01 table for 2");
+            comboboxTableNumber.Items.Add("A02 table for 2");
+            comboboxTableNumber.Items.Add("A03 table for 2");
+            comboboxTableNumber.Items.Add("A04 table for 2");
+            comboboxTableNumber.Items.Add("A05 table for 2");
+            comboboxTableNumber.Items.Add("B01 table for 4");
+            comboboxTableNumber.Items.Add("B02 table for 4");
+            comboboxTableNumber.Items.Add("B03 table for 4");
+            comboboxTableNumber.Items.Add("B04 table for 4");
+            comboboxTableNumber.Items.Add("B05 table for 4");
+            comboboxTableNumber.Items.Add("C01 table for 6");
+            comboboxTableNumber.Items.Add("C02 table for 6");
+            comboboxTableNumber.Items.Add("C03 table for 6");
+            comboboxTableNumber.Items.Add("C04 table for 6");
+            comboboxTableNumber.Items.Add("C05 table for 6");
+            comboboxTableNumber.Items.Add("D01 table for 8");
+            comboboxTableNumber.Items.Add("D02 table for 8");
+            comboboxTableNumber.Items.Add("D03 table for 8");
+            comboboxTableNumber.Items.Add("D04 table for 8");
+            comboboxTableNumber.Items.Add("D05 table for 8");
+            comboTime.Items.Add("11:30");
+            comboTime.Items.Add("12:00");
+            comboTime.Items.Add("12:30");
+            comboTime.Items.Add("13:00");
+            comboTime.Items.Add("17:30");
+            comboTime.Items.Add("18:00");
+            comboTime.Items.Add("18:30");
+            comboTime.Items.Add("19:00");
+
+            if (System.IO.File.Exists(xmlPath))
+            {
+                loadXmlFile();
+            }
+            else
+            {
+                XDocument doc = new XDocument(new XElement("Booking"));
+                doc.Save(xmlPath);
+            }
+            
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.CellSelect;
+            dataGridView1.DefaultCellStyle.SelectionBackColor = Color.IndianRed;
+            dataGridView1.DefaultCellStyle.SelectionForeColor = Color.Black;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+        }
+
+
+
+
+
+
+        //show bookings with details in table 
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            bookingID = dataGridView1.Rows[e.RowIndex].Cells[0].Value.ToString();
+            XElement data = XElement.Load(xmlPath);
+            IEnumerable<XElement> elements = from bookingInfo in data.Elements("Booking")
+                                             where bookingInfo.Attribute("ID").Value == bookingID
+                                             select bookingInfo;
+            foreach (XElement element in elements)
+            {
+                txtBookingID.Text = bookingID;
+                txtname.Text = element.Element("Name").Value;
+                txtPhone.Text = element.Element("Phone").Value;
+                numPeople.Text = element.Element("Num_People").Value;
+                dateTimePicker1.Text = element.Element("Date").Value;
+                comboTime.Text = element.Element("Time").Value;
+                comboboxTableNumber.Text = element.Element("Table_Num").Value;
+            }
+        }
+
+
+
+        //add 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            XElement data = XElement.Load(xmlPath);
+            IEnumerable<XElement> elementsAdd = from element in data.Elements("Booking")
+                                             select element;
+            int i = 0;
+            string str = (Convert.ToInt32(elementsAdd.Max(element => element.Attribute("ID").Value)) + (i+1)).ToString("00");
+            string strOne = str;
+            try
+            {
+
+                XElement booking = new XElement(
+                "Booking",new XAttribute("ID",str),
+                            new XElement("BookingID", str),
+                            new XElement("Name",txtname.Text),
+                            new XElement("Phone",txtPhone.Text),                       
+                            new XElement("Num_People",numPeople.Text),
+                            new XElement("Date",dateTimePicker1.Text),
+                            new XElement("Time", comboTime.Text),
+                            new XElement("Table_Num",comboboxTableNumber.Text)               
+                );
+
+                if (txtname.Text != "" && txtPhone.Text != "") { 
+
+                    data.Add(booking);
+                    data.Save(xmlPath);
+                    loadXmlFile();
+                }
+                else {
+                    MessageBox.Show("Please enter all blank fileds, then click Add Button!Thank you");
+                }
+
+            }
+            catch (System.Exception)
+            {
+
+            }
+
+           
+        }
+
+
+        //update
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (bookingID != "")
+            { 
+                XElement data = XElement.Load(xmlPath);
+                IEnumerable<XElement> elements = from element in data.Elements("Booking")
+                                                 where element.Attribute("ID").Value == bookingID
+                                                 select element;          
+                if (elements.Count() > 0)
+                {
+                    XElement UpdateData = elements.First();
+                    UpdateData.SetAttributeValue("ID", bookingID);
+                    UpdateData.ReplaceNodes(
+                        new XElement("BookingID", bookingID),
+                        new XElement("Name", txtname.Text),
+                        new XElement("Phone", txtPhone.Text),
+                        new XElement("Num_People", numPeople.Text),
+                        new XElement("Date", dateTimePicker1.Text),
+                        new XElement("Time", comboTime.Text),
+                        new XElement("Table_Num", comboboxTableNumber.Text)
+                        );
+                }
+                data.Save(xmlPath);
+            }
+            MessageBox.Show("Selected Booking Information has Updated");
+            loadXmlFile();
+        }
+
+        //delete
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (bookingID != "")
+            {
+                XElement data = XElement.Load(xmlPath);
+                IEnumerable<XElement> elements = from element in data.Elements("Booking")
+                                                  where element.Attribute("ID").Value == bookingID
+                                                  select element;
+                if (elements.Count() > 0)
+                    elements.First().Remove();
+                data.Save(xmlPath);
+            }
+            MessageBox.Show("Selected Booking has Deleted");
+            loadXmlFile();
+        }
+
+
+        //search
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (txtSeacrchName.Text != "")
+            {
+                Name = txtSeacrchName.Text;
+                
+
+                for (int i = 0; i < dataGridView1.Rows.Count - 1; i++)
+                {
+                    string s = dataGridView1.Rows[i].Cells[1].Value.ToString();
+                    if (s == Name)
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.LightCyan;
+                        dataGridView1.Rows[i].DefaultCellStyle.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.White;
+                        dataGridView1.Rows[i].DefaultCellStyle.ForeColor = Color.Black;
+                    }
+                }
+            }
+            else
+                MessageBox.Show("Please Enter a Customer name for Search");
+        }
+
+
+        //reset for new bookings
+        private void btnnew_Click(object sender, EventArgs e)
+        {
+            this.txtBookingID.Text = "";
+            this.txtname.Text = "";
+            this.txtPhone.Text = "";
+            this.numPeople.Text = "";
+            this.dateTimePicker1.Text = "";
+            this.comboTime.Text = "";
+            this.comboboxTableNumber.Text = "";
+        }
+        private void txtname_TextChanged(object sender, EventArgs e)
+        {
+
+            //check customer name must be starting with a character 
+            if (this.txtname.Text.Length <= 1)
+            {
+                foreach (char ch in this.txtname.Text)
+                {
+                    if (char.IsDigit(ch))
+                    {
+                        MessageBox.Show("Please Enter a valid Name, starting with character Only!" + '\n'+ "Enter exp: MAO or Qiang!");
+                    }
+                    
+                }
+            }
+        }
+
+        private void txtPhone_TextChanged(object sender, EventArgs e)
+        {
+            //check phone number must be numbers
+            if (this.txtPhone.Text.Length <= txtPhone.Text.Length)
+            {
+                foreach (char ch in this.txtPhone.Text)
+                {
+                    if (char.IsLetter(ch))
+                    {                    
+                        MessageBox.Show("Please Enter a valid number, Enter exp: 02240147800");
+                    }
+
+                }
+            }
+
+        }
+
+        private void numPeople_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            dateTimePicker1.MinDate = DateTime.Now;
+            
+        }
+
+
+
+        private void tableNumber_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSeacrchName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
